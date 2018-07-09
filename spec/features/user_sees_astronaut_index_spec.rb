722f554e137876acb3e9c27e464e522548679504
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-08 22:56:21 -0600

    All tests passing

--- spec/features/user_sees_astronaut_index_spec.rb
+++ spec/features/user_sees_astronaut_index_spec.rb
@@ -1,9 +1,11 @@
+require 'rails_helper'
+
 describe 'user sees all astronauts' do
-  it "sees all astronauts' info" do
+  it "astronauts' info" do
     astronaut_1 = Astronaut.create(name: "Buzz Lightyear of Star Command", age: 36, job: "Defender of Peace in Space!")
     astronaut_2 = Astronaut.create(name: "Warp Darkmatter", age: 340, job: "Little is known about this mysterious alien")
 
-    visit '/astronauts'
+    visit astronauts_path
 
     expect(page).to have_content(astronaut_1.name)
     expect(page).to have_content(astronaut_1.age)
@@ -11,5 +13,47 @@ describe 'user sees all astronauts' do
     expect(page).to have_content(astronaut_2.name)
     expect(page).to have_content(astronaut_2.age)
     expect(page).to have_content(astronaut_2.job)
+  end
+
+  it 'sees average age' do
+    astronaut_1 = Astronaut.create(name: "Buzz Lightyear of Star Command", age: 10, job: "Defender of Peace in Space!")
+    astronaut_2 = Astronaut.create(name: "Warp Darkmatter", age: 20, job: "Little is known about this mysterious alien")
+    expected_result = 15
+
+    visit astronauts_path
+
+    expect(page).to have_content(expected_result)
+  end
+end
+
+describe 'user sees all astronaut missions' do
+  it 'lists missions alphabetically' do
+    astronaut_1 = Astronaut.create!(name: "Buzz Lightyear of Star Command", age: 10, job: "Defender of Peace in Space!")
+    mission_1 = SpaceMission.create!(title: 'Blow up the Death Star', trip_length: 5)
+    mission_2 = SpaceMission.create!(title: 'Eat Cereal', trip_length: 10)
+    mission_3 = SpaceMission.create!(title: 'Win a space pie-eating contest', trip_length: 15)
+    AstronautSpaceMission.create!(astronaut_id: astronaut_1.id, space_mission_id: mission_1.id)
+    AstronautSpaceMission.create!(astronaut_id: astronaut_1.id, space_mission_id: mission_2.id)
+    AstronautSpaceMission.create!(astronaut_id: astronaut_1.id, space_mission_id: mission_3.id)
+
+    visit astronauts_path
+
+    expect(page).to have_content(mission_1.title)
+    expect(page).to have_content(mission_2.title)
+    expect(page).to have_content(mission_3.title)
+  end
+  it 'lists total time astronaut has spent in space' do
+    astronaut_1 = Astronaut.create!(name: "Buzz Lightyear of Star Command", age: 10, job: "Defender of Peace in Space!")
+    mission_1 = SpaceMission.create!(title: 'Blow up the Death Star', trip_length: 5)
+    mission_2 = SpaceMission.create!(title: 'Eat Cereal', trip_length: 10)
+    mission_3 = SpaceMission.create!(title: 'Win a space pie-eating contest', trip_length: 15)
+    AstronautSpaceMission.create!(astronaut_id: astronaut_1.id, space_mission_id: mission_1.id)
+    AstronautSpaceMission.create!(astronaut_id: astronaut_1.id, space_mission_id: mission_2.id)
+    AstronautSpaceMission.create!(astronaut_id: astronaut_1.id, space_mission_id: mission_3.id)
+    (expected_result = 30)
+
+    visit astronauts_path
+
+    expect(page).to have_content(expected_result)
   end
 end
--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -5,4 +5,27 @@ describe Astronaut, type: :model do
     it {should have_many(:astronaut_space_missions)}
     it {should have_many(:space_missions).through(:astronaut_space_missions)}
   end
+
+  describe 'calculations' do
+    it 'finds average age of astronauts' do
+      astronaut_1 = Astronaut.create(name: "Buzz Lightyear of Star Command", age: 10, job: "Defender of Peace in Space!")
+      astronaut_2 = Astronaut.create(name: "Warp Darkmatter", age: 20, job: "Little is known about this mysterious alien")
+
+      expected_result = 15
+
+      expect(Astronaut.average_age).to eq(expected_result)
+    end
+    it 'finds an astronaut total time in space' do
+      astronaut_1 = Astronaut.create(name: "Buzz Lightyear of Star Command", age: 10, job: "Defender of Peace in Space!")
+      mission_1 = SpaceMission.create!(title: 'Blow up the Death Star', trip_length: 5)
+      mission_2 = SpaceMission.create!(title: 'Eat Cereal', trip_length: 10)
+      mission_3 = SpaceMission.create!(title: 'Win a space pie-eating contest', trip_length: 15)
+      AstronautSpaceMission.create!(astronaut_id: astronaut_1.id, space_mission_id: mission_1.id)
+      AstronautSpaceMission.create!(astronaut_id: astronaut_1.id, space_mission_id: mission_2.id)
+      AstronautSpaceMission.create!(astronaut_id: astronaut_1.id, space_mission_id: mission_3.id)
+      result = 30
+
+      expect(astronaut_1.total_time_in_space).to eq(30)
+    end
+  end
 end
